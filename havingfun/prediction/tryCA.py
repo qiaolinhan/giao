a7f7436bcb350ba12348dd8854f8ad34f97271ca
qiaolinhan <742954173@qq.com>
2021-11-05 14:31:41 -0400

    original code not working?

--- havingfun/prediction/tryCA.py
+++ havingfun/prediction/tryCA.py
@@ -1,13 +1,13 @@
 # a grid of 100x00 cells
-# each cell can be in 3 (5) states: clear, fuel, `heating`, buruning, `ash`
+# each cell can be in 5 states: clear, vegetation, heating, buruning, flamed
 # if cell is burning, it will ignite the heating around it.
 import numpy as np
 import imageio
 
 # cell states
-# 0 = clear, 1 = fuel, 2 = heating, burning = 3, ash = 4
+# 0 = clear, 1 = vegetation, 2 = heating, burning = 3, flamed = 4
 # probability of a cell can being fuel, or it is clear
-prob_f = 0.7
+prob_f = 0.6
 p = [prob_f, 1-prob_f]
 # simulation time
 total_time = 300
@@ -20,7 +20,7 @@ states = np.zeros((total_time, *terrian_size))
 states[0] = fild
 
 # set the middle cell on fire
-states[0, terrian_size[0]//2, terrian_size[1]//2] = 3
+states[0, terrian_size[0]//2, terrian_size[1]//2] = 2
 
 for t in range(1, total_time-1):
     states[t] = states[t-1].copy()
@@ -28,7 +28,7 @@ for t in range(1, total_time-1):
     for x in range(1, terrian_size[0]-1):
         for y in range(1, terrian_size[1]-1):
             # if it is burning, put it out into ash
-            if states[t-1, x, y] == 3:
+            if states[t-1, x, y] == 2:
                 states[t, x, y] == 3
                 states[t+1, x, y]== 4
 
@@ -41,9 +41,6 @@ for t in range(1, total_time-1):
                     states[t, x, y+5] == 2
                 if states[t-1, x, y-5] ==1:
                     states[t, x, y-5] == 2
-
-            if states[t-1, x, y] == 2:
-                states[t, x, y] == 3
 
 colored = np.zeros((total_time, *terrian_size, 3), dtype = np.uint8)
 
@@ -63,50 +60,6 @@ for t in range(states.shape[0]):
             elif value == 4:
                 colored[t, x, y] = [96, 96, 96]
 
-croped = colored[:200, 1:terrian_size[0]-1, 1:terrian_size[1]-1]
+croped = colored[:300, 1:terrian_size[0]-1, 1:terrian_size[1]-1]
 
 imageio.mimsave('./predict.gif', croped)
-
-
-# def ca():
-#     # celluar automata
-#     # list representing the current status of 64 cells
-#    ca = [
-#          0,0,0,0,0,0,0,0,0,0, 0,0,0,0,0,0,0,0,0,0,
-#          0,0,0,0,0,0,0,0,0,0, 0,1,0,0,0,0,0,0,0,0,
-#          0,0,0,0,0,0,0,0,0,0, 0,0,0,0,0,0,0,0,0,0,  0,0,0,0]
-
-#     ca_new = ca[:]
-
-#     dic = {0: 'F', 1: 'T'}
-
-#     #initial draw, step 0
-#     print ''.join(dic[e] for e in ca_new)
-#     step = 1
-#     while(step <32):
-#         ca_new = []
-#         for i in range(0, 64):
-#             if i>0 and i<63:
-#                 if ca[i-1] == ca[i+1]:
-#                     ca_new.append(0)
-#                 else:
-#                     ca_new.append(1)
-
-#             elif(i==0):
-#                 if ca[1] == 1:
-#                     ca_new.append(1)
-#                 else:
-#                     ca_new.append(0)
-
-#             elif(i==63):
-#                 if ca[62] ==1:
-#                     ca_new.append(1)
-#                 else:
-#                     ca_new.append(0)
-
-#     # draw current cell state
-#     print ''.join([dic[e] for e in ca_new])
-
-#     ca - ca_new[:]
-
-#     step += 1
