3e2c691932189f90a52d031ca005161e6d6a2f57
qiaolinhan <742954173@qq.com>
2021-11-17 11:09:44 -0500

    worling classifier

--- havingfun/classifier/resnet34_model.py
+++ havingfun/classifier/resnet34_model.py
@@ -1,3 +1,22 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*- #
+
+# ------------------------------------------------------------------------------
+#
+#   Copyright (C) 2021 Concordia NAVlab. All rights reserved.
+#
+#   @Filename: resnet34_model.py
+#
+#   @Author: Linhan Qiao
+#
+#   @Date: 2021-11-11
+#
+#   @Email:
+#
+#   @Description:
+#
+# ------------------------------------------------------------------------------
+
 import torch
 import torch.nn as nn
 from torch.nn.modules.activation import ReLU
@@ -8,17 +27,22 @@ class Conv0(nn.Module):
     def __init__(self, in_channels, out_channels):
         super(Conv0, self).__init__()
         self.conv0 = nn.Sequential(
-            nn.Conv2d(in_channels, out_channels, kernel_size=7, stride=2, padding=3, bias=False),
+            nn.Conv2d(in_channels, out_channels, kernel_size=7,
+                      stride=2, padding=3, bias=False),
             nn.BatchNorm2d(out_channels),
             nn.ReLU(),
             nn.MaxPool2d(kernel_size=3, stride=2),
         )
-    def forward(self, x):   
+
+    def forward(self, x):
         return self.conv0(x)
+
 # conv layer in residual blocks
+
+
 class Block(nn.Module):
     def __init__(self, in_channels, out_channels):
-        super(Block,self).__init__()
+        super(Block, self).__init__()
         self.block = nn.Sequential(
             nn.Conv2d(in_channels, out_channels, 3, 1, 1),
             nn.BatchNorm2d(out_channels),
@@ -27,41 +51,54 @@ class Block(nn.Module):
             nn.BatchNorm2d(out_channels)
         )
         self.res = nn.Sequential(
-             nn.Conv2d(in_channels, out_channels, 1,1,0, bias = False),
-             nn.BatchNorm2d(out_channels)
+            nn.Conv2d(in_channels, out_channels, 1, 1, 0, bias=False),
+            nn.BatchNorm2d(out_channels)
         )
         self.relu = nn.ReLU(inplace=True)
 
     def forward(self, x):
         residual = self.res(x)
-        print('residual shape:', residual.shape)
+        # print('residual shape:', residual.shape)
         x = self.block(x)
-        print('blocked shape:', x.shape)
+        # print('blocked shape:', x.shape)
         x += residual
         x = self.relu(x)
         return x
 
-# # test: to match the blocked shapes
-# x_block = torch.randn((1, 3, 255, 255))
-# block = Block(in_channels = 3, out_channels = 64)
-# print(block.eval())
-# preds = block(x_block)
-# print('preds shape:', preds.shape)
-# print('input shape:', x_block.shape)
-
-
 # consist resnet blocks together (18 or 34)
+
+
 class Resnet34(nn.Module):
     def __init__(self, img_channels, num_classes):
         super(Resnet34, self).__init__()
-        self.inputconv = Conv0(img_channels, out_channels = 64)
+        self.inputconv = Conv0(img_channels, out_channels=64)
 
-        structure = [3, 4, 6, 3]
-        self.layer1 = nn.ModuleList(Block(in_channels=64, out_channels=64) for i in range(structure[0]))
-        self.layer2 = nn.ModuleList(Block(in_channels=64, out_channels=128) for i in range(structure[1]))
-        self.layer3 = nn.ModuleList(Block(in_channels=128, out_channels=256) for i in range(structure[2]))
-        self.layer4 = nn.ModuleList(Block(in_channels=256, out_channels=512) for i in range(structure[3]))
-        
+        # structure = [3, 4, 6, 3]
+        self.layer1 = nn.Sequential(
+            Block(in_channels=64, out_channels=64),
+            Block(in_channels=64, out_channels=64),
+            Block(in_channels=64, out_channels=64),
+        )
+        self.layer2 = nn.Sequential(
+            Block(in_channels=64, out_channels=128),
+            Block(in_channels=128, out_channels=128),
+            Block(in_channels=128, out_channels=128),
+            Block(in_channels=128, out_channels=128),
+        )
+        self.layer3 = nn.Sequential(
+            Block(in_channels=128, out_channels=256),
+            Block(in_channels=256, out_channels=256),
+            Block(in_channels=256, out_channels=256),
+            Block(in_channels=256, out_channels=256),
+            Block(in_channels=256, out_channels=256),
+            Block(in_channels=256, out_channels=256),
+        )
+        self.layer4 = nn.Sequential(
+            Block(in_channels=256, out_channels=512),
+            Block(in_channels=512, out_channels=512),
+            Block(in_channels=512, out_channels=512),
+        )
+
         self.avgpool = nn.AdaptiveAvgPool2d((1, 1))
         self.fc = nn.Linear(512, num_classes)
 
@@ -78,11 +115,13 @@ class Resnet34(nn.Module):
         output = self.fc(ap_out)
         return output
 
-# ##########################
-# test whether model is okay
-img = torch.randn((1, 3, 255, 255)) # batchsize = 1, channels = 3, inputsize = 255*255
-model = Resnet34(img_channels=3, num_classes=3)
-print(model.eval())
-preds = model(img)
-print('preds shape:', preds.shape)
-print('input shape:', img.shape)
+
+if __name__ == '__main__':
+
+    # batchsize = 1, channels = 3, inputsize = 255*255
+    img = torch.randn((4, 3, 255, 255))
+    model = Resnet34(img_channels=3, num_classes=3)
+    print(model.eval())
+    preds = model(img)
+    print('input shape:', img.shape)
+    print('preds shape:', preds.shape)
