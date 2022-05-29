b592ccc084e7c663877118617340d3617b5522b8
qiaolinhan <742954173@qq.com>
2022-05-29 10:14:47 -0400

    .pyed

--- havingfun/detecting/segmentation/unet34/attunet_res34_testing.py
+++ havingfun/detecting/segmentation/unet34/attunet_res34_testing.py
@@ -19,6 +19,7 @@
 #
 # ------------------------------------------------------------------------------
 
+# import some dependencies
 import os
 import cv2
 
@@ -30,23 +31,35 @@ import torch
 import torch.nn as nn
 import matplotlib.pyplot as plt
 
+import pathlib
+from datetime import datetime
+
+from tqdm import tqdm
+
+from PIL import Image
+from PIL import ImageEnhance
+# ----------------------------------
+# load the original dataset which is used for building the learner
+
 # the dataset path
 path = Path('/home/qiao/dev/giao/datasets/')
 
 # load the images path
 path_img = path/'S_kaggle_wildfire/'
 fnames = get_image_files(path_img)
+# test whether loaded the data correctly
 print(f'======> image path: {path_img}')
 print(f'======> loaded the images: {fnames[0]}')
 
 # load the label path
 path_label = path/'S_kaggle_wildfire_label'
-codes = ['Smoke', 'Flame', 'Cloud', 'Background']
+codes = ['Smoke', 'Flame', 'Background']
+
+# ---------------------------------------------
+# for building the dataloader
 
 # connection between images and masks names
 get_y_fn = lambda x: path_label/f'label_{x.name}'
-
-
 # building the dataloader for learner
 dls = SegmentationDataLoaders.from_label_func(
         path_img,
@@ -81,8 +94,30 @@ learn.model.to(Device)
 # load the trained parameters
 learn.load('/home/qiao/dev/giao/datasets/bounding/params/attunet')
 
+# ----------------------------------
+# testing part start
 # loading test image folder
 test_path = '/home/qiao/dev/giao/datasets/bonding/images'
+test_fnames = get_image_files(test_path)
+total_number = len(test_fnames)
+# check whether loaded the testing dataaset correctly
+print(f'======> A testing dataset example: {test_fnames[0]}')
+
+# do some convertion just in case it is needed to enhance the original images
+# in this work, [brightness, color balance, contrast] are considered, other ratios are also adjust-able.
+def enh_img(img):
+    # brightness
+    enh_bri = ImageEnhance.Brightness(img)
+    img_enhanced = enh_bri.enhance(factor = 1.0)
+
+    # ecolor balance
+    enh_col = ImageEnhance.Color(img_enhanced)
+    img_enhanced = enh_col.enhance(factor = 1.0)
+
+    # contrast
+    enh_con = ImageEnhance.Contrast(img_enhanced)
+    img_enhanced = enh_con.enhance(factor = 1.0)
+    return img_enhanced
 
 def load_image_from_folder(folder_name):
     images = []
