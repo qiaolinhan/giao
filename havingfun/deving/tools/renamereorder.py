d7de990d75a9f21e96e45ee0977d530820a4a5c4
qiaolinhan <742954173@qq.com>
2022-05-31 15:45:56 -0400

    rename 20220526

--- havingfun/deving/tools/renamereorder.py
+++ havingfun/deving/tools/renamereorder.py
@@ -1,13 +1,30 @@
 import os
 import sys
 
-os.chdir('dataset/imgs/jinglingseg1/masks')
+# rename images
+os.chdir('/home/qiaolinhan/dev/datasets/M300S/m300dataset20220526/imgs')
 print(os.getcwd())
 
 for count, f in enumerate(os.listdir()):
     f_name, f_ext = os.path.splitext(f)
-    print(f_name)
-    f_name = 'mask_img' + str(count)
+    print(f'{f_name} + {f_ext}')
 
+    f_name = 'M300_' + str(count) + '_20220526'
+    print(f'======> new name {f_name} ')
     new_name = f'{f_name}{f_ext}'
     os.rename(f, new_name)
+    print(f'======> renamed, please check')
+
+# rename labels
+os.chdir('/home/qiaolinhan/dev/datasets/M300S/m300dataset20220526/labels')
+print(os.getcwd())
+
+for count, f in enumerate(os.listdir()):
+    f_name, f_ext = os.path.splitext(f)
+    print(f'{f_name} + {f_ext}')
+
+    f_name = 'label_' + 'M300_' + str(count) + '_20220526'
+    print(f'======> new name {f_name} ')
+    new_name = f'{f_name}{f_ext}'
+    os.rename(f, new_name)
+    print(f'======> renamed, please check')
