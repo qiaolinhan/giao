8c0431a117a890ff0e208530c1be9c2dc36eed14
qiaolinhan <742954173@qq.com>
2022-06-01 12:05:59 -0400

    labeling and rename

--- havingfun/deving/tools/renamereorder.py
+++ havingfun/deving/tools/renamereorder.py
@@ -2,29 +2,30 @@ import os
 import sys
 
 # rename images
-os.chdir('/home/qiaolinhan/dev/datasets/M300S/m300dataset20220526/imgs')
+os.chdir('/mnt/g/datasets/geolocating/20220601/renamed/17.1/imgs/')
 print(os.getcwd())
 
 for count, f in enumerate(os.listdir()):
     f_name, f_ext = os.path.splitext(f)
     print(f'{f_name} + {f_ext}')
 
-    f_name = 'M300_' + str(count) + '_20220526'
+    f_name = 'M300_' + '20220601_' + '51.2_' + str(count)
     print(f'======> new name {f_name} ')
     new_name = f'{f_name}{f_ext}'
     os.rename(f, new_name)
     print(f'======> renamed, please check')
 
 # rename labels
-os.chdir('/home/qiaolinhan/dev/datasets/M300S/m300dataset20220526/labels')
+os.chdir('/mnt/g/datasets/geolocating/20220601/renamed/17.1/labels/')
 print(os.getcwd())
 
 for count, f in enumerate(os.listdir()):
     f_name, f_ext = os.path.splitext(f)
     print(f'{f_name} + {f_ext}')
 
-    f_name = 'label_' + 'M300_' + str(count) + '_20220526'
+    f_name = 'label_' + 'M300_' + '20220601_' + '51.2_' + str(count)
     print(f'======> new name {f_name} ')
+
     new_name = f'{f_name}{f_ext}'
     os.rename(f, new_name)
     print(f'======> renamed, please check')
