55b4be1672618a8a526b7c49553a6c303f553974
qiaolinhan <742954173@qq.com>
2022-06-02 11:50:24 -0400

    bounding pass, making videos

--- havingfun/deving/tools/renamereorder.py
+++ havingfun/deving/tools/renamereorder.py
@@ -1,6 +1,9 @@
 import os
 import sys
 
+# mount the device
+# sudo mount -t drvfs E: /mnt/g
+# sudo umount /mnt/g
 # rename images
 os.chdir('/mnt/g/datasets/geolocating/20220601/renamed/17.1/imgs/')
 print(os.getcwd())
@@ -9,7 +12,7 @@ for count, f in enumerate(os.listdir()):
     f_name, f_ext = os.path.splitext(f)
     print(f'{f_name} + {f_ext}')
 
-    f_name = 'M300_' + '20220601_' + '51.2_' + str(count)
+    f_name = 'M300_' + '20220601_' + '17.1_' + str(count)
     print(f'======> new name {f_name} ')
     new_name = f'{f_name}{f_ext}'
     os.rename(f, new_name)
@@ -23,7 +26,7 @@ for count, f in enumerate(os.listdir()):
     f_name, f_ext = os.path.splitext(f)
     print(f'{f_name} + {f_ext}')
 
-    f_name = 'label_' + 'M300_' + '20220601_' + '51.2_' + str(count)
+    f_name = 'label_' + 'M300_' + '20220601_' + '17.1_' + str(count)
     print(f'======> new name {f_name} ')
 
     new_name = f'{f_name}{f_ext}'
