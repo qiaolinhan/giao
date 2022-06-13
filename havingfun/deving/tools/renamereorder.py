2fb174e5f8c0f2647b691242734bd50e6a272ef5
qiaolinhan <742954173@qq.com>
2022-06-13 18:23:38 -0400

    sort and rename

--- havingfun/deving/tools/renamereorder.py
+++ havingfun/deving/tools/renamereorder.py
@@ -1,29 +1,44 @@
 import os
 import sys
 
-# rename images
-os.chdir('/mnt/g/datasets/geolocating/20220601/renamed/17.1/imgs/')
-print(os.getcwd())
+# # rename images
+# os.chdir('/home/qiaolinhan/dev/giao/datasets/M300S/extended/imgs')
+# print(os.getcwd())
+# file_list = os.listdir()
 
-for count, f in enumerate(os.listdir()):
-    f_name, f_ext = os.path.splitext(f)
-    print(f'{f_name} + {f_ext}')
+# # reorder: sort the files by name
+# def last_11chars(x):
+#     return(x[-4:])
 
-    f_name = 'M300_' + '20220601_' + '51.2_' + str(count)
-    print(f'======> new name {f_name} ')
-    new_name = f'{f_name}{f_ext}'
-    os.rename(f, new_name)
-    print(f'======> renamed, please check')
+# print(sorted(file_list, key = last_11chars))  
+
+# for count, f in enumerate(sorted(file_list, key = last_11chars)):
+#     f_name, f_ext = os.path.splitext(f)
+#     print(f'{f_name} + {f_ext}')
+
+#     f_name = 'M300_' + '20220613_' + str(count)
+#     print(f'======> new name {f_name} ')
+#     new_name = f'{f_name}{f_ext}'
+#     os.rename(f, new_name)
+#     print(f'======> renamed, please check')
 
 # rename labels
-os.chdir('/mnt/g/datasets/geolocating/20220601/renamed/17.1/labels/')
+os.chdir('/home/qiaolinhan/dev/giao/datasets/M300S/extended/41.2/labels')
 print(os.getcwd())
 
-for count, f in enumerate(os.listdir()):
+file_list = os.listdir()
+
+# reorder: sort the files by name
+def last_11chars(x):
+    return(x[-8:])
+filelist_sorted = sorted(file_list, key = last_11chars)
+print(filelist_sorted)  
+
+for count, f in enumerate(filelist_sorted):
     f_name, f_ext = os.path.splitext(f)
     print(f'{f_name} + {f_ext}')
 
-    f_name = 'label_' + 'M300_' + '20220601_' + '51.2_' + str(count)
+    f_name = 'label_' + 'M300_' + '20220613_' + '41.2_' + str(count)
     print(f'======> new name {f_name} ')
 
     new_name = f'{f_name}{f_ext}'
