1e2249b3c7412f6f4ea8b50e668ed5661d1a5cb0
qiaolinhan <742954173@qq.com>
2022-07-30 10:39:47 -0400

    super init

--- havingfun/detecting/segmentation/tutorial_training_with_torch.py
+++ havingfun/detecting/segmentation/tutorial_training_with_torch.py
@@ -68,13 +68,13 @@ def matplotlib_imshow(img, one_channel = False):
     if one_channel:
         img = img.mean(dim = 0)
     img = img / 2 + 0.5 # unormalize
-    npimg = img.numpy() # fogot the '()' first time
+    npimg = img.numpy() # don't forget the '()'
     if one_channel:
         plt.imshow(npimg, cmap = "Greys")
-        plt.show() # added this for avoiding the warning
+        # plt.show() # added this for avoiding the warning
     else:
         plt.imshow(np.transpose(npimg, (1, 2, 0)))
-        plt.show() # added this for avoiding the warning
+        # plt.show() # added this for avoiding the warning
 
 dataiter = iter(training_loader)
 images, labels = dataiter.next()
@@ -83,3 +83,33 @@ images, labels = dataiter.next()
 img_grid = torchvision.utils.make_grid(images)
 matplotlib_imshow(img_grid, one_channel = True)
 print('======> This is a anity check '.join(classes[labels[j]] for j in range(4)))
+
+# ----------------------------------
+'''
+The Model
+The Model used in this example is avariant of LeNet5
+'''
+import torch.nn as nn
+import torch.nn.functional as F
+
+# PyTorch models inherint from torch.nn.model
+class GarmentClassifier(nn.Module):
+    def __init__(self):
+        super(GarmentClassifier, self).__init__() # don't forget super().__init__()
+        self.conv1 = nn.Conv2d(1, 6, 5)
+        self.pool = nn.MaxPool2d(2, 2)
+        self.conv2 = nn.Conv2d(6, 16, 5)
+        self.fc1 = nn.Linear(16 * 4 * 4, 120)
+        self.fc2 = nn.Linear(120, 84)
+        self.fc3 = nn.Linear(84, 10)
+
+    def forward(self, x):
+        x = self.pool(F.relu(self.conv1(x)))
+        x = self.pool(F.relu(self.conv2(x)))
+        x = x.view(-1, 16 * 4 * 4)
+        x = F.relu(self.fc1(x))
+        x = F.relu(self.fc2(x))
+        x = self.fc3(x)
+        return x
+
+model = GarmentClassifier()
