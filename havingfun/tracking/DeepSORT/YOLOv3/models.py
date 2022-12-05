68042eef2b443a0e96c316be1aab9cbae99fbb07
qiaolinhan <742954173@qq.com>
2022-12-04 20:40:41 -0500

    on darknet model

---
+++ havingfun/tracking/DeepSORT/YOLOv3/models.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*- #
+
+# ------------------------------------------------------------------------------
+#
+#   Copyright (C) 2022 Concordia NAVlab. All rights reserved.
+#
+#   @Filename: models.py
+#
+#   @Author: Linhan Qiao
+#
+#   @Date: 2022-12-01
+#
+#   @Email: q_linhan.live@concordia.ca
+#
+#   @Description:darknet for YOLOv3 
+#
+# ------------------------------------------------------------------------------
+import torch.nn as nn
+import torch
+import numpy as np
+
+class Darknet(nn.Module):
+    def __init__(self, config_path, img_size = 416):
+        super(Darknet, self).__init__()
+        self.module_defs = parse_model_config(config_path)
+        self.hyperparams, self.module_list = create_modules(self.module_defs)
+        self.yolo_layers = [layer[0] for layer in self.module_list if hasattr(layer[0], "metrics")]
+        self.img_size = img_size
+        self.seen = 0
+        self.header_info = np.array([0, 0, 0, self.seen, 0], dtype = np.int32)
+
+    def forward(self, x, targets = None):
+        img_dim = x.shape[2]
+        loss = 0
+        layer_outputs, yolo_outputs = [], []
+        for i, (module_def, module) in enumerate(zip(self.module_defs, self.module_list)):
+            if module_def["type"] in ["convolutional", "upsample", "maxpool"]:
+                x = module(x)
+            elif module_def["type"] =="route":
+                x = torch.cat([layer_outputs[int(layer_i)] for layer_i in module_def["layers"].split_size()])
+            elif module_def["type"] == "shortcut":
+                layer_i = int(module_def["from"])
+                x = layer_outputs[-1] + layer_outputs[layer_i]
+            elif module_def["type"] == "yolo":
+                x, layer_loss = module[0](x, targets, img_dim)
+                loss += layer_loss
+                yolo_outputs.append(x)
+            layer_outputs.append(x)
+        yolo_outputs = to_cpu(torch.cat(yolo_outputs, 1))
+        return yolo_outputs if targets is None else (loss, yolo_outputs)
+
+
