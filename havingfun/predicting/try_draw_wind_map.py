e853cd1d1d3aac4b50551edaa597dd0644628851
qiaolinhan <742954173@qq.com>
2022-12-12 17:17:54 -0500

    wind map u

--- havingfun/predicting/try_draw_wind_map.py
+++ havingfun/predicting/try_draw_wind_map.py
@@ -34,6 +34,10 @@
     #               fontproperties = {'size': 10, 'family': 'Times New Roman'}
     # )
 # ------------------------------------------------------------------------------
+
+import os
+import sys
+
 import matplotlib.pyplot as plt
 import numpy as np
 import netCDF4 as nc
@@ -47,37 +51,51 @@ import cartopy.feature as cfeat
 from wrf import getvar, to_np
 import cmaps
 
+# To show 'Chinese correctly'
 Simsun = FontProperties(fname = "./font/Simsun.ttf")
-Times = FontProperties(fname = "./font/Times.ttf")
+Times = FontProperties(fname = "./font/times.ttf")
 config = {
         "font.family": 'serif',
         "mathtext.fontset": 'stix',
-        "font.serif": ['SimSun'],
+        "font.serif": [],
         }
-
 mpl.rcParams.update(config)
+
+# To show '-' correctly
 mpl.rcParams['axes.unicode_minus'] = False
 
 fig = plt.figure(figsize = (5,5), dpi = 150)
 axe = plt.subplot(1, 1, 1, projection = ccrs.PlateCarree())
-axe.set_title('Wind Map', fontsize = 12, y = 1.05)
+axe.set_title('$\mathrm{Wind\ Map}$', fontsize = 12, y = 1.05)
 
-axe.add_feature(cfeat.COASTLINE.with_scale('10m'), linewidth = 1, color = 'k')
+axe.add_feature(cfeat.COASTLINE.with_scale('10m'), linewidth = 0.5, color = 'k')
 LAKES_border = cfeat.NaturalEarthFeature('pysical', 'lakes', '10m', edgecolor = 'k', facecolor = 'never')
 axe.add_feature(LAKES_border, linewidth=0.8)
+# to add more features
+axe.add_feature(cfeat.LAND.with_scale('10m'))
+axe.add_feature(cfeat.OCEAN.with_scale('10m'))
+axe.add_feature(cfeat.RIVERS.with_scale('10m'))
 
 # The plot area of the map
-axe.set_extent([-90, -60, 40, 50], crs = ccrs.PlateCarree())
-gl = axe.gridlines(crs = ccrs.PlateCarree(), draw_labels = True, linewidth = 0.8, color = 'gray', linestyle = ':')
-gl.top_labels, gl.bottom_labels, gl.right_labels, gl.left_labels = False, False, False, False
-gl.xlocator = mticker.FixedLocator(np.arange(-90, -60, 0.5))
-gl.ylocator = mticker.FixedLocator(np.arange(40, 50, 0.5))
-axe.set_xticks(np.arange(-90, -60, 0.5), crs = ccrs.PlateCarree())
-axe.set_yticks(np.arange(40, 50, 0.5), crs = ccrs.PlateCarree())
+axe.set_extent([-75, -68, 44, 47], crs = ccrs.PlateCarree())
+gl = axe.gridlines(crs = ccrs.PlateCarree(), draw_labels = True, linewidth = 0.1, color = 'gray', linestyle = ':')
+# gl.top_labels, gl.bottom_labels, gl.right_labels, gl.left_labels = False, False, False, False
+gl.xlocator = mticker.FixedLocator(np.arange(-74.7, -68.1, 0.1))
+gl.ylocator = mticker.FixedLocator(np.arange(44.9, 46.4, 0.1))
+axe.set_xticks(np.arange(-75, -68, 0.1), crs = ccrs.PlateCarree())
+axe.set_yticks(np.arange(44, 47, 0.1), crs = ccrs.PlateCarree())
 axe.xaxis.set_major_formatter(LongitudeFormatter())
 axe.yaxis.set_major_formatter(LatitudeFormatter())
-axe.tick_params(labelcolor = 'k', length = 5)
+axe.tick_params(labelcolor = 'k', length = 2)
 labels = axe.get_xticklabels() + axe.get_yticklabels()
 [label.set_fontproperties(FontProperties(fname = "./font/Times.ttf", size = 8)) for label in labels]
 
+ncfile = nc.Dataset('~/dev/giao/data/prediction_use/POWER_Point_Daily_20210101_20210301_045d1537N_073d7012W_LST.nc')
+ua = getvar(ncfile, 'ua', timeidx = 126)[0, :, :]
+va = getvar(ncfile, 'va', timeidx = 126)[0, :, :]
+lat = getvar(ncfile, 'lat')
+lon = getvar(ncfile, 'lon')
+quiver = axe.quiverkey(quiver, 0.91, 1.03, 3, "3m/s", 
+                       labelpos = "E", coordinates = "axes", 
+                       fontproperties = {"size": 10, "family":"Times New Roman"})
 plt.show()
