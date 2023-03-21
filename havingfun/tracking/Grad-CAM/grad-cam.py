244ebb84b5ba8b838aa0d502fa443a0109ae1152
qiaolinhan <742954173@qq.com>
2023-03-21 15:56:17 -0400

    grad-cam u

---
+++ havingfun/tracking/Grad-CAM/grad-cam.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*- #
+
+# ------------------------------------------------------------------------------
+#
+#   Copyright (C) 2023 Concordia NAVlab. All rights reserved.
+#
+#   @Filename: grad-cam.py
+#
+#   @Author: Linhan Qiao
+#
+#   @Date: 2023-03-21
+#
+#   @Email: q_linhan.live@concordia.ca
+#
+#   @Description: 
+#
+# ------------------------------------------------------------------------------
+
+import numpy as np
+
