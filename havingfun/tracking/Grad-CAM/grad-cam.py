7c1d1de74c4855af6a4067ed7188dcf1ff055bb7
qiaolinhan <742954173@qq.com>
2023-03-23 10:44:40 -0400

    grad-cam u

--- havingfun/tracking/Grad-CAM/grad-cam.py
+++ havingfun/tracking/Grad-CAM/grad-cam.py
@@ -18,4 +18,29 @@
 # ------------------------------------------------------------------------------
 
 import numpy as np
+import torch
+import torchvision 
+import cv2
+
+from matplotlib import pyplot as plt 
+from PIL import Image 
+
+# input image  
+
+# Find the target layer
+# e.g.: In the ResNet50, there are multiple CNN blocks from layer1 to layer4.
+#       Trying to use the block layer4 and get the last layer from it.  
+
+from torchvision.transforms.transforms import ToPILImage
+from torchvision import transforms 
+model = torchvision.models.resnet50(pretrained = True)
+model.eval()
+print('======> Print the model:\n', model)
+
+target_layer = model.layer4[-1]
+
+pic = cv2.imread('img_path', 1)
+img = pic.copy()
+Covert_RGB2GBR
+
 
