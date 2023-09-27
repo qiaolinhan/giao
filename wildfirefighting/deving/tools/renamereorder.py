c769b5097fa07650e0299d6de3626b8b286321bf
qiaolinhan <742954173@qq.com>
2023-09-27 15:30:18 -0400

    tool: rename reorder updated

--- wildfirefighting/deving/tools/renamereorder.py
+++ wildfirefighting/deving/tools/renamereorder.py
@@ -1,67 +1,72 @@
 import os
 import sys
 
-<<<<<<< HEAD
-# mount the device
+########################
+# # For dataset on Ejectable device
+# # Mount the device
 # sudo mount -t drvfs E: /mnt/g
 # sudo umount /mnt/g
-# rename images
-os.chdir('/mnt/g/datasets/geolocating/20220601/renamed/17.1/imgs/')
-print(os.getcwd())
-=======
 # # rename images
-# os.chdir('/home/qiaolinhan/dev/giao/datasets/M300S/extended/imgs')
+# os.chdir('/mnt/g/datasets/geolocating/20220601/renamed/17.1/imgs/')
 # print(os.getcwd())
-# file_list = os.listdir()
->>>>>>> 46c2817fa7f5b56f3bae574dc3f4093af1cc8687
-
-# # reorder: sort the files by name
-# def last_11chars(x):
-#     return(x[-4:])
-
-<<<<<<< HEAD
-    f_name = 'M300_' + '20220601_' + '17.1_' + str(count)
-    print(f'======> new name {f_name} ')
-    new_name = f'{f_name}{f_ext}'
-    os.rename(f, new_name)
-    print(f'======> renamed, please check')
-=======
-# print(sorted(file_list, key = last_11chars))  
-
-# for count, f in enumerate(sorted(file_list, key = last_11chars)):
-#     f_name, f_ext = os.path.splitext(f)
-#     print(f'{f_name} + {f_ext}')
-
-#     f_name = 'M300_' + '20220613_' + str(count)
-#     print(f'======> new name {f_name} ')
-#     new_name = f'{f_name}{f_ext}'
-#     os.rename(f, new_name)
-#     print(f'======> renamed, please check')
->>>>>>> 46c2817fa7f5b56f3bae574dc3f4093af1cc8687
-
-# rename labels
-os.chdir('/home/qiaolinhan/dev/giao/datasets/M300S/extended/41.2/labels')
+########################
+# For dataet copied to pc
+# load the datas from a specific folder
+os.chdir('/home/qiao/dev/giao/wildfirefighting/deving/tools/data/mask/')
 print(os.getcwd())
-
 file_list = os.listdir()
+print("[INFO] Successfully read the folder:", os.getcwd())
+print("[INFO] Sample name of the file in this folder:", file_list[0])
 
+########################
 # reorder: sort the files by name
 def last_11chars(x):
-    return(x[-8:])
+    return(x[-7:])
+# Sort the files 
 filelist_sorted = sorted(file_list, key = last_11chars)
-print(filelist_sorted)  
+# print("[INFO] The dataset is sorted\n", filelist_sorted)  
+########################
+
+# new_name = str(count)
+# print(f'[INFO] New name {new_name}')
+# new_name = f'{f_name}{f_ext}'
+# os.rename(f, new_name)
+# print(f'[INFO] Renamed accomplished')
+# print(sorted(file_list, key = last_11chars))  
 
 for count, f in enumerate(filelist_sorted):
     f_name, f_ext = os.path.splitext(f)
-    print(f'{f_name} + {f_ext}')
+    print(f'[INFO] Original name: {f_name} + {f_ext}')
+    
+    # new name 
+    # f_name = 'M300_' + '20220613_' + str(count)
+    # use '{:03}'.format(num) to zero lead the num
+    f_name_n = "%04d"%count
+    new_name = f'{f_name_n}{f_ext}'
+    print(f'[INFO] New name {f_name_n} + {f_ext}')
 
-<<<<<<< HEAD
-    f_name = 'label_' + 'M300_' + '20220601_' + '17.1_' + str(count)
-=======
-    f_name = 'label_' + 'M300_' + '20220613_' + '41.2_' + str(count)
->>>>>>> 46c2817fa7f5b56f3bae574dc3f4093af1cc8687
-    print(f'======> new name {f_name} ')
-
-    new_name = f'{f_name}{f_ext}'
     os.rename(f, new_name)
-    print(f'======> renamed, please check')
+    print(f'[INFO] renamed, please check')
+
+# # rename labels
+# os.chdir('/home/qiaolinhan/dev/giao/datasets/M300S/extended/41.2/labels')
+# print(os.getcwd())
+
+# file_list = os.listdir()
+
+# # reorder: sort the files by name
+# def last_11chars(x):
+#     return(x[-8:])
+# filelist_sorted = sorted(file_list, key = last_11chars)
+# print(filelist_sorted)  
+
+# for count, f in enumerate(filelist_sorted):
+#     f_name, f_ext = os.path.splitext(f)
+#     print(f'{f_name} + {f_ext}')
+
+#     f_name = 'label_' + 'M300_' + '20220601_' + '17.1_' + str(count)
+#     print(f'======> new name {f_name} ')
+
+#     new_name = f'{f_name}{f_ext}'
+#     os.rename(f, new_name)
+#     print(f'======> renamed, please check')
