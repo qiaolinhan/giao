efdd8495f241313dd5e2bbd1bcaf3129545772bb
qiaolinhan <742954173@qq.com>
2023-09-28 11:54:54 -0400

    Tool: videocutter update

--- works/deving/tools/Tool_VideoCutter.py
+++ works/deving/tools/Tool_VideoCutter.py
@@ -17,23 +17,39 @@
 #
 # ------------------------------------------------------------------------------
 import cv2
+import numpy as np
 import os
 from pathlib import Path
 import glob
 
 path = Path('/home/qiao/dev/giao/data/videos/')
-file_posix = path/'20230926/20230926Howie.mp4'
+file_posix = path/'20230926/20230926.mp4'
 file_path = str(file_posix)
-print('[INFO] The file path is:', file_path)
+# print('[INFO] The file path is:', file_path)
 
 # load the video with cv2
 video = cv2.VideoCapture(file_path)
 
+########################################
+## play the video
+#while video.isOpened():
+#    ret, frame = video.read()
+#    if not ret:
+#        print("[WARNING] Can not receive frame (stream end?).Existing ...")
+#        break
+#    gray = cv2.cvtColor(video, cv2.COLOR_BGR2GRAY)
+#    cv2.imshow('frame', gray)
+#    if cv2.waitKey(1) == ord('q'):
+#        break
+#video.release()
+#cv2.destroyAllWindows()
+#######################################
+
 # get the FPS information of the video, also check if we successfully loaded the video
 fps = video.get(cv2.CAP_PROP_FPS)
-print('fps', fps)
+print('[INFO] Fps of the video', fps)
 frame_count = video.get(cv2.CAP_PROP_FRAME_COUNT)
-print('frame_count', frame_count)
+print('[INFO] Frame_count of the video', frame_count)
 # calculate the duration, how long the video is, through 'duration = frame_count/fps'
 duration = frame_count / fps
 print('[INFO] Frame per seconds (FPS) of this video is %d'%fps)
@@ -56,10 +72,12 @@ print('[INFO] The specific frame_id at {}:{}:{} is {}'.format(hours, minutes, se
 #cv2.imwrite('saved_frame.png', frame)
 ################################
 
-# # cut the video and save into the folder
+# cut the video and save into the folder
 # for i in range(0, frame_id):
-#     video.set(cv2.CAP_PROP_POS_FRAMES, i)
-#     ret, frame = video.read()
-#     cv2.imwrite('/home/qiao/dev/giao/datasets/test_RW_20230828/frame_%d.png'%i, frame)
+for i in range(0, 1816):
 
-# print('[INFO] The video is cut into frames and saved in folder')
+    video.set(cv2.CAP_PROP_POS_FRAMES, i)
+    ret, frame = video.read()
+    cv2.imwrite('/home/qiao/dev/giao/data/videos/20230926/frames/frame_%d.png'%i, frame)
+
+print('[INFO] The video is cut into frames and saved in folder')
