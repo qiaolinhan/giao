402965355f0787e1695dfef3dedd91b3cea33c1b
qiaolinhan <742954173@qq.com>
2024-02-07 18:14:54 -0500

    compile openCV Done

--- works/fusion/reinstall_opencv.sh
+++ works/fusion/reinstall_opencv.sh
@@ -15,59 +15,77 @@ sudo apt upgrade -y
 #
 # to install Java JDK
 # ######################
-sudo apt install default-jre -y
+sudo apt install default-jdk
+sudo apt install default-jre
 # sudo apt install openjdk-11-jre-headless -y
 # sudo apt install openjdk-17-jre-headless -y
 # sudo apt install openjdk-18-jre-headless -y
 # sudo apt install openjdk-9-jre-headless -y
-
-java --version
+java -version
+update-alternatives --config java
+# which java
+# sudo nvim /etc/environment
+# add the java path
+# (/usr/lib/jvm/java-11-openjdk-amd64/bin/java)
+# source /etc/environment
+echo $JAVA_HOME
 
 # to install Apache Ant
 #  #####################
-cd ~/Downloads
+sudo apt install ant
+ant -version
+
+wget https://dlcdn.apache.org//ant/binaries/apache-ant-1.10.14-bin.tar.gz
+tar -xvzf apache-ant-1.10.14-bin.tar.gz
+sudo mv apache-ant-1.10.14 ~/apache-ant
+ # sudo nvim /etc/environment
+# add the java path
+# (/opt/apache-ant/bin)
+# source /etc/environment
 
 # Set environmental variables: JAVA_HOME to your Java environment, ANT_HOME to the directory you uncompressed Ant to, and add ${ANT_HOME}/bin (Unix) or %ANT_HOME%\bin (Windows) to your PATH. See Setup for details.
 
-
 # to compile opencv
 # #######################
-wget -c https://github.com/opencv/opencv/archive/4.5.1.tar.gz
-tar -xvzf 4.5.1.tar.gz && rm -rf 4.5.1.tar.gz
+wget -c https://github.com/opencv/opencv/archive/refs/tags/4.9.0.tar.gz
+tar -xvzf 4.9.0.tar.gz
+sudo mv opencv-4.9.0/ /opt/opencv4
+rm -rf 4.9.0.tar.gz
 
-wget -c https://github.com/opencv/openc_contrib/archive/4.5.1.tar.gz
-tar -xvzf 4.5.1.tar.gz && rm -rf 4.5.1.tar.gz
+wget -c https://github.com/opencv/opencv_contrib/archive/refs/tags/4.9.0.tar.gz
+tar -xvzf 4.9.0.tar.gz
+sudo mv opencv_contrib-4.9.0/ /opt/opencv4_contrib
+rm -rf 4.9.0.tar.gz
 
-sudo mv opencv-4.5.1/ /opt/opencv4
-sudo mv opencv_contrib-4.5.1/ /opt/opencv4_contrib
 
 cd /opt/opencv4
 rm -rf build
 mkdir build && cd build
 
 # select the path of conda environment
-conda_env_path=~/anaconda3/envs/yolov8
+conda_env_path=~/anaconda3/envs/dev
 opencv_contrib_path=/opt/opencv4_contrib
 
 # specifying OPENCV_ENABLE_NONFREE=ON and conda environment
 cmake   -D CMAKE_BUILD_TYPE=RELEASE \
 	-D CMAKE_INSTALL_PREFIX=/opt/opencv/ \
-        -D OPENCV_EXTRA_MODULES_PATH=$opencv_contrib_path/modules \
-        -D PYTHON3_EXECUTABLE=$conda_env_path/bin/python3.12 \
-        -D PYTHON3_LIBRARY=$conda_env_path/lib/python3.12 \
-        -D PYTHON3_INCLUDE_DIR=$conda_env_path/include/python3.12 \
-        -D PYTHON3_PACKAGES_PATH=$conda_env_path/lib/python3.12/site-packages \
-        -D BUILD_opencv_python2=OFF \
-        -D BUILD_opencv_python3=ON \
-        -D INSTALL_PYTHON_EXAMPLES=OFF \
-        -D INSTALL_C_EXAMPLES=OFF \
-        -D OPENCV_ENABLE_NONFREE=ON \
-        -D BUILD_EXAMPLES=OFF ..
-	
+	-D OPENCV_EXTRA_MODULES_PATH=$opencv_contrib_path/modules \
+	-D PYTHON3_EXECUTABLE=$conda_env_path/bin/python3.11 \
+	-D PYTHON3_LIBRARY=$conda_env_path/lib/libpython3.11.so \
+	-D PYTHON3_NUMPY_INCLUDE_DIRS=$conda_env_path/lib/python3.11/site-packages/numpy/core/include \
+	-D PYTHON3_PACKAGES_PATH=$conda_env_path/lib/python3.11/site-packages \
+	-D BUILD_opencv_python2=OFF \
+	-D BUILD_opencv_python3=ON \
+	-D INSTALL_PYTHON_EXAMPLES=OFF \
+	-D INSTALL_C_EXAMPLES=OFF \
+	-D OPENCV_ENABLE_NONFREE=ON \
+	-D BUILD_EXAMPLES=OFF ..
+# To check whether there are ANT, JDK, java-wrappers, ...
+
 # set the number of cores for installation and compile
 make -j4
 sudo make install
 
 # link the shared library to the conda environment
-cd ~/anaconda3/envs/yolov8/lib/python3.12
-ln -s /opt/opencv4/build/lib/python3/cv2.cpython-38-x86_64-linux-gnu.so
+# cd ~/anaconda3/envs/dev/lib/python3.11
+# ln -s /opt/opencv4/build/lib/python3/dist-packages/cv2.cpython-38-x86_64-linux-gnu.so
